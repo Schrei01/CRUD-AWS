1e992f9106e2aba3d43d18041c508547212f9c7e
Alejandro Vargas <alejovarf@gmail.com>
2022-09-12 12:34:02 -0500

    Petición get para una sola tarea
    Se agrega la función de traer una sola tarea

---
+++ src/getTask.js
@@ -0,0 +1,25 @@
+const AWS =  require('aws-sdk');
+
+const getTask = async (event) => {
+
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+    const {id} = event.pathParameters
+
+    const result = await dynamodb.get({
+        TableName: 'TaskTable',
+        Key: {
+            id
+        }
+    }).promise();
+
+    const task = result.Item
+
+    return {
+        status: 200,
+        body: task
+    }
+}
+
+module.exports = {
+    getTask
+}
